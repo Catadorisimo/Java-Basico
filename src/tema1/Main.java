5f37d1a423e146c81bd96c6f5fe9850350cbdfcc
Suso García <88231627+Catadorisimo@users.noreply.github.com>
2022-07-30 12:00:41 +0200

    Tema 1 ejercicio 1

---
+++ src/tema1/Main.java
@@ -0,0 +1,29 @@
+package tema1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean t = true;
+
+        char c = 'a';
+        String str = "abc";
+
+        byte b = 1;
+        int i = 123;
+        long l = 1234567890;
+
+        float f = 2.5F;
+        double d = 3.6;
+
+        System.out.println(
+                " Boolean: " + t + "\n" +
+                " Char: " + c + "\n" +
+                " String: " + str + "\n" +
+                " Byte: " + b + "\n" +
+                " Int: " + i + "\n" +
+                " Long: " + l + "\n" +
+                " Float: " + f + "\n" +
+                " Double: " + d
+        );
+    }
+}
