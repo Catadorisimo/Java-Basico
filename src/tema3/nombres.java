9bb58732ba4652a5216be47b1e9750815d193e1d
Suso García <88231627+Catadorisimo@users.noreply.github.com>
2022-07-30 13:34:08 +0200

    Tema 3 ejercicio 1

---
+++ src/tema3/nombres.java
@@ -0,0 +1,18 @@
+package tema3;
+
+public class nombres {
+    public static void main(String[] args) {
+        String[] nombres = {"nombre1", "nombre2", "nombre3", "nombre4"};
+        String str = "";
+        for (String nombre:nombres){
+            str += nombre +" ";
+        }
+
+        System.out.println(str);
+
+    }
+
+
+
+
+}
